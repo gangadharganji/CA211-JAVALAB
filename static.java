a33a49f23c12f30c780fd81feba731814bf8e9af
gangadharganji <69528209+gangadharganji@users.noreply.github.com>
2020-11-27 13:36:18 +0530

    Add files via upload

---
+++ static.java
@@ -0,0 +1,20 @@
+class lab10
+{
+static int count=0;
+static void lab10()
+{
+count++;
+}
+public static void main(strinf args[])
+{
+lab10 c1=new lab10();
+c1.lab10();
+lab10c2=new lab10();
+c2.lab10();
+lab10c3=new lab10();
+c3.lab10();
+lab10c4=new lab10();
+c4.lab10();
+system .out.println("number of objects="+count);
+}
+}
